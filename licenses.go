1227b525c4444f63d697c9b98d80582c126eb5fe
kraxarn <me@kraxarn.com>
2019-12-21 15:44:01 +0100

    Update licenses

--- licenses.go
+++ licenses.go
@@ -2,74 +2,51 @@ package main
 
 // This file only contains the licenses for the used libraries
 
-const licenses =
-	"go:\n" +
-		"Copyright (c) 2009 The Go Authors. All rights reserved.\n\n" +
-		"Redistribution and use in source and binary forms, with or without\n" +
-		"modification, are permitted provided that the following conditions are\n" +
-		"met:\n\n" +
-		"* Redistributions of source code must retain the above copyright\n" +
-		"notice, this list of conditions and the following disclaimer.\n" +
-		"* Redistributions in binary form must reproduce the above\n" +
-		"copyright notice, this list of conditions and the following disclaimer\n" +
-		"in the documentation and/or other materials provided with the\n" +
-		"distribution.\n" +
-		"* Neither the name of Google Inc. nor the names of its\n" +
-		"contributors may be used to endorse or promote products derived from\n" +
-		"this software without specific prior written permission.\n\n" +
-		"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n" +
-		"\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT\n" +
-		"LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR\n" +
-		"A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT\n" +
-		"OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,\n" +
-		"SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT\n" +
-		"LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,\n" +
-		"DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY\n" +
-		"THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n" +
-		"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n" +
-		"OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n" +
-		"fyne:\n" +
-		"Copyright (C) 2018 Fyne.io developers (see AUTHORS)\n" +
-		"All rights reserved.\n\n" +
-		"Redistribution and use in source and binary forms, with or without\n" +
-		"modification, are permitted provided that the following conditions are met:\n" +
-		"* Redistributions of source code must retain the above copyright\n" +
-		"notice, this list of conditions and the following disclaimer.\n" +
-		"* Redistributions in binary form must reproduce the above copyright\n" +
-		"notice, this list of conditions and the following disclaimer in the\n" +
-		"documentation and/or other materials provided with the distribution.\n" +
-		"* Neither the name of Fyne.io nor the names of its contributors may be\n" +
-		"used to endorse or promote products derived from this software without\n" +
-		"specific prior written permission.\n\n" +
-		"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND\n" +
-		"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED\n" +
-		"WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
-		"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY\n" +
-		"DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES\n" +
-		"(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;\n" +
-		"LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND\n" +
-		"ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n" +
-		"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS\n" +
-		"SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n" +
-		"grab:\n" +
-		"Copyright (c) 2017 Ryan Armstrong. All rights reserved.\n\n" +
-		"Redistribution and use in source and binary forms, with or without modification,\n" +
-		"are permitted provided that the following conditions are met:\n\n" +
-		"1. Redistributions of source code must retain the above copyright notice, this\n" +
-		"list of conditions and the following disclaimer.\n" +
-		"2. Redistributions in binary form must reproduce the above copyright notice,\n" +
-		"this list of conditions and the following disclaimer in the documentation\n" +
-		"and/or other materials provided with the distribution.\n\n" +
-		"3. Neither the name of the copyright holder nor the names of its contributors\n" +
-		"may be used to endorse or promote products derived from this software without\n" +
-		"specific prior written permission.\n\n" +
-		"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND\n" +
-		"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED\n" +
-		"WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
-		"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR\n" +
-		"ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES\n" +
-		"(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;\n" +
-		"LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON\n" +
-		"ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n" +
-		"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS\n" +
-		"SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
+const licenses = "go:\n" +
+	"Copyright (c) 2009 The Go Authors. All rights reserved.\n\n" +
+	"Redistribution and use in source and binary forms, with or without\n" +
+	"modification, are permitted provided that the following conditions are\n" +
+	"met:\n\n" +
+	"* Redistributions of source code must retain the above copyright\n" +
+	"notice, this list of conditions and the following disclaimer.\n" +
+	"* Redistributions in binary form must reproduce the above\n" +
+	"copyright notice, this list of conditions and the following disclaimer\n" +
+	"in the documentation and/or other materials provided with the\n" +
+	"distribution.\n" +
+	"* Neither the name of Google Inc. nor the names of its\n" +
+	"contributors may be used to endorse or promote products derived from\n" +
+	"this software without specific prior written permission.\n\n" +
+	"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n" +
+	"\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT\n" +
+	"LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR\n" +
+	"A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT\n" +
+	"OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,\n" +
+	"SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT\n" +
+	"LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,\n" +
+	"DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY\n" +
+	"THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n" +
+	"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n" +
+	"OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n" +
+	"fyne:\n" +
+	"Copyright (C) 2018 Fyne.io developers (see AUTHORS)\n" +
+	"All rights reserved.\n\n" +
+	"Redistribution and use in source and binary forms, with or without\n" +
+	"modification, are permitted provided that the following conditions are met:\n" +
+	"* Redistributions of source code must retain the above copyright\n" +
+	"notice, this list of conditions and the following disclaimer.\n" +
+	"* Redistributions in binary form must reproduce the above copyright\n" +
+	"notice, this list of conditions and the following disclaimer in the\n" +
+	"documentation and/or other materials provided with the distribution.\n" +
+	"* Neither the name of Fyne.io nor the names of its contributors may be\n" +
+	"used to endorse or promote products derived from this software without\n" +
+	"specific prior written permission.\n\n" +
+	"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND\n" +
+	"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED\n" +
+	"WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
+	"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY\n" +
+	"DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES\n" +
+	"(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;\n" +
+	"LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND\n" +
+	"ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n" +
+	"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS\n" +
+	"SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
